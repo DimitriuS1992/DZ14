4302de41d82354507b8bf88a4c13813edb2de3ec
Dimitrius1992 <dim.tupowskij@mail.ru>
2019-07-12 21:01:48 +0300

    DZ14

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+   
+    function gallery(){
     var leftArrow = document.getElementById('leftArrow');
     var rightArrow = document.getElementById('rightArrow');
-    function gallery(){
-   
-    initImgNumber = 1,
-    maxImgNumber = 3;
+    var initImgNumber = 1;
+    var maxImgNumber = 3;
     
     leftArrow.addEventListener('click', moveLeft);
     rightArrow.addEventListener('click', moveRight);
     
     function moveLeft(){
-        alert('left');
+       
         var image = document.getElementById('image');
-       initImgNumber--;
+        initImgNumber--;
         if(initImgNumber == 0){initImgNumber = maxImgNumber};
         image.setAttribute('src', 'img/' + initImgNumber + '.jpg');
     };
     
     function moveRight(){
-        alert('right');
+        
         var image = document.getElementById('image');
         initImgNumber++;
         if(initImgNumber > maxImgNumber){initImgNumber = 1};
